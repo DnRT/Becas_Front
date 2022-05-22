6a4b4c4b7f43e1e1692c26743d323ae1dd3f6649
DnRT <eugenio.cortes@alumnos.uv.cl>
2022-05-22 19:02:31 -0400

    Logica registro

--- logicaRegistro.js
+++ logicaRegistro.js
@@ -1,18 +1,22 @@
 function registrar(){
-    var datos = {
-        'Rut':document.getElementById('rut').value,
-        'Nombre':document.getElementById('nombre').value,
-        'Apellido':document.getElementById('apellido').value,
-        'Correo':document.getElementById('email').value,
-        'Contrasena':document.getElementById('pass').value
-    };
-    console.log(datos);
-    $.ajax({
-        type: 'POST',
-        url: 'http://127.0.0.1:8000/api/test/alumnos',
-        data:datos,
-        success: function(data){
-            console.log(data);
+    var nuevo_registro = document.getElementById('nuevo_registro');
+    var datos = new FormData(nuevo_registro);
+    datos.append('rut',document.getElementById('rut').value);
+    datos.append('nombre',document.getElementById('nombre').value);
+    datos.append('apellido',document.getElementById('apellido').value);
+    datos.append('edad',null);
+    datos.append('direccion',null);
+    datos.append('correo',document.getElementById('email').value);
+    datos.append('contrasena',document.getElementById('pass').value);
+    fetch('http://127.0.0.1:8000/api/test/alumnos', {
+        method:'POST',
+        body:datos
+    }).then(function(response){
+        if(response.ok){
+            window.location.href='./loginvista.html';
+        }
+        else{
+            alert(response.statusText);
         }
     })
 }
