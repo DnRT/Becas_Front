0d87b2386bd27fe61490a9fcab739673bae83974
DnRT <eugenio.cortes@alumnos.uv.cl>
2022-07-20 23:07:14 -0400

    avance 20/07

--- logicaRegistro.js
+++ logicaRegistro.js
@@ -1,18 +1,28 @@
 function registrar(){
     var datos = {
-        'Rut':document.getElementById('rut').value,
-        'Nombre':document.getElementById('nombre').value,
-        'Apellido':document.getElementById('apellido').value,
-        'Correo':document.getElementById('email').value,
-        'Contrasena':document.getElementById('pass').value
+        'rut':document.getElementById('rut').value,
+        'nombre':document.getElementById('nombre').value,
+        'apellido':document.getElementById('apellido').value,
+        'correo':document.getElementById('correo').value,
+        'contrasena':document.getElementById('clave').value,
+        'edad':document.getElementById('edad').value,
+        'direccion':document.getElementById('dirección').value
     };
-    console.log(datos);
     $.ajax({
         type: 'POST',
         url: 'http://127.0.0.1:8000/api/test/alumnos',
         data:datos,
         success: function(data){
             console.log(data);
+        },
+        error: function(data){
+            let aux = Object.values(data.responseJSON.errors);
+            aux.forEach(test =>{
+                test.forEach(value=> {
+                    alert(value);
+                });
+            });
         }
+        
     })
 }
