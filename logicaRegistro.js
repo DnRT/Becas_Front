d9bb8340ba5af0ff0644ab439f828c509208a66d
DnRT <eugenio.cortes@alumnos.uv.cl>
2022-07-22 15:52:39 -0400

    solucion error registro alumno

--- logicaRegistro.js
+++ logicaRegistro.js
@@ -6,22 +6,21 @@ function registrar(){
         'correo':document.getElementById('correo').value,
         'contrasena':document.getElementById('clave').value,
         'edad':document.getElementById('edad').value,
-        'direccion':document.getElementById('dirección').value
+        'direccion':document.getElementById('dirección').value,
+        'id_beca':0,
+        'ubicacion_archivos':'../archivos/'+document.getElementById('nombre').value+document.getElementById('apellido').value
     };
+    console.log(datos);
     $.ajax({
         type: 'POST',
         url: 'http://127.0.0.1:8000/api/test/alumnos',
         data:datos,
         success: function(data){
             console.log(data);
+            window.location.href='./loginvista.html';
         },
         error: function(data){
-            let aux = Object.values(data.responseJSON.errors);
-            aux.forEach(test =>{
-                test.forEach(value=> {
-                    alert(value);
-                });
-            });
+            alert(data.statusText);
         }
         
     })
